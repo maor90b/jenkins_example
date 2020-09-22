9f60cd8102bc81fd1dcfd60bfab7fc363d1dfd2e
maor90b <68278384+maor90b@users.noreply.github.com>
2020-09-22 11:17:00 +0300

    Update loop4.4.py

--- loop4.4.py
+++ loop4.4.py
@@ -1,11 +1,8 @@
-num = int(input('enter number: '))    #קולט מספר שאורכו לא ידוע. הופך את המספר ומדפיס את המספר ההפוך וגם כפול 2
-new_num = 0
+from time import sleep
+import sys
 
-while num//10 != 0:
-    new_num += num%10
-    new_num *= 10
-    num //= 10
-
-new_num += num
-print(new_num)
-print(new_num*2)
+if __name__=="__main__":
+    inuser=int(sys.argv[1])
+    for i in range(inuser):
+    	print(i,i**3)
+    	sleep(1)
